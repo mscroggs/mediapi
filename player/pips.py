7d7235643b11c1e7ac48ae175b91cc3472888d2c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-03-25 21:23:31 +0000

    Add force

--- player/pips.py
+++ player/pips.py
@@ -5,9 +5,9 @@ import sys
 song = sys.argv[1]
 
 p = player.MusicPlayer()
-p.set_volume(120)
 p.set_media(song)
-while datetime.now().minute == 59 and datetime.now().second < 54:
+p.set_volume(120)
+while datetime.now().minute == 59 and datetime.now().second < 54 and "force" not in sys.argv:
     continue
 p.play()
 while not p.has_ended():
