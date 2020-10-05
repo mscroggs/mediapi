b25fb01bfde97bb960a2f20c66316a1e782cd3f1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-10-05 21:07:37 +0100

    Setup CI

---
+++ player/config.py
@@ -0,0 +1,12 @@
+import os as _os
+
+_dir = _os.path.dirname(_os.path.realpath(__file__))
+music_dir = _os.path.join(_dir, "../Music")
+db_dir = _os.path.join(_dir, "db")
+data_dir = _os.path.join(_dir, "../data")
+player_dir = _dir
+
+try:
+    from local_config import *
+except:
+    pass
